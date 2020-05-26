acd5f5c9235225e9f095fb2bbb1b386bd4ef2ef9
Nikolas Bompetsis <nbompetsis@di.uoa.gr>
2020-05-27 01:24:31 +0300

    Refactoring in order to read data from csv

--- public/bar.js
+++ public/bar.js
@@ -1,42 +1,55 @@
-const DUMMY_DATA = [
-    { gender: 'MAN', value: 1000},
-    { gender: 'WOMAN' , value: 500}
-];
-
 const height = 250
 const width = 300
 const margin = 30
 
-const xScale = d3.scaleBand().domain(DUMMY_DATA.map((d) => d.gender)).rangeRound([0, 300]).padding(0.1);
-const yScale = d3.scaleLinear().domain([0, 1100]).range([250, 0]);
-
 const container = d3.select('#bar_chart')
               .append("svg")
-              .attr("width", width + 100)
+              .attr("width", width + margin)
               .attr("height", height + margin);
-    
-            container.append("g") 
+
+d3.csv("./data/sample_10.csv",
+  function(data, i) {
+    return {
+      age : data.Age,
+      gender : data.Gender
+    };
+  })
+  .then(data => {
+    const plotData = []
+    data.forEach(a => {
+      if (!plotData.some(d => d.gender === a.gender)){
+        const obj = {};
+        obj['gender'] = a.gender;
+        obj['value'] = 1;
+        plotData.push(obj);
+      } else {
+        const objIndex = plotData.findIndex((obj => obj.gender === a.gender));
+        plotData[objIndex]['value'] = plotData[objIndex]['value'] + 1;
+      }
+    });
+
+    const maxValue = plotData.reduce((prev, current) => (prev.value > current.value) ? prev : current);
+    const xScale = d3.scaleBand().domain(plotData.map((d) => d.gender)).rangeRound([0, width]).padding(0.1);
+    const yScale = d3.scaleLinear().domain([0, maxValue.value + 1]).range([height, 0]);
+
+    container.append("g") 
               .attr("transform", "translate(0,250)")
               .call(d3.axisBottom(xScale));
 
-            // container.append("g")
-            //   .attr("transform", "translate(35,0)")
-            //   .call(d3.axisLeft(yScale));
-
-const bar = container
-              .selectAll('.bar')
-              .data(DUMMY_DATA)
-              .enter()
-              .append('rect')
-              .classed('bar', true)
-              .attr('width', xScale.bandwidth())
-              .attr('height', data => height - yScale(data.value))
-              .attr('x', data => xScale(data.gender))
-              .attr('y', data => yScale(data.value))
-              .style("fill", "#69b3a2")
-              .style("opacity", 0.5)
-            container.append("text")      // text label for the x axis
+    container.selectAll('.bar')
+            .data(plotData)
+            .enter()
+            .append('rect')
+            .classed('bar', true)
+            .attr('width', xScale.bandwidth())
+            .attr('height', data => height - yScale(data.value))
+            .attr('x', data => xScale(data.gender))
+            .attr('y', data => yScale(data.value))
+            .style("fill", "#69b3a2")
+            .style("opacity", 0.5)
+    container.append("text")
               .attr("x", width/2 )
               .attr("y",  height + margin)
               .style("text-anchor", "middle")
               .text("Gender");
+  });
